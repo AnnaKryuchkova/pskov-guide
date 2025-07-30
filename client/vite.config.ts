1fafd2c06f949bab7ba266c4fed1b111c27cb2b4
Anna Kryuchkova <Anytkathefli@gmail.com>
2025-07-30 14:31:24 +0300

    fix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,9 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   publicDir: 'public',
+  build: {
+    outDir: 'dist', // ← Должно быть относительно client/, а не корня
+  }, // соберет файлы в корень проекта
   plugins: [
     react(),
     svgr(), // Добавьте плагин

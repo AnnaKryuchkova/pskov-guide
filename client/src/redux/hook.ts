baf6c960948ea7cac7d581ef39d1bbf264996301
Anna Kryuchkova <Anytkathefli@gmail.com>
2025-07-30 14:20:39 +0300

    fix

--- client/src/redux/hook.ts
+++
@@ -1,8 +0,0 @@
-import type { TypedUseSelectorHook } from 'react-redux';
-import { useDispatch, useSelector } from 'react-redux';
-import type { RootState, AppDispatch } from './store';
-
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-export const useAppDispatch: () => AppDispatch = useDispatch;
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;

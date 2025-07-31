963a6640349e1c1341552fba11fc19dd85be273d
Anna Kryuchkova <Anytkathefli@gmail.com>
2025-07-31 13:35:43 +0300

    я устал честн говоря очоч

--- server/init-db.js
+++ server/init-db.js
@@ -1,6 +1,6 @@
-const { execSync } = require('child_process');
+// const { execSync } = require('child_process');
 
-console.log('Starting database initialization...');
-execSync('npx sequelize-cli db:migrate', { stdio: 'inherit' });
-execSync('npx sequelize-cli db:seed:all', { stdio: 'inherit' });
-console.log('Database initialization complete');
+// console.log('Starting database initialization...');
+// execSync('npx sequelize-cli db:migrate', { stdio: 'inherit' });
+// execSync('npx sequelize-cli db:seed:all', { stdio: 'inherit' });
+// console.log('Database initialization complete');
